f4d8413575f98e398b444e1f7ae8bd5da9f8dab7
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2022-02-21 08:51:28 -0300

    o método __len__()

--- extrator-url.py
+++ extrator-url.py
@@ -41,6 +41,12 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
-extrator_url = ExtratorURL("bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar")
+    def __len__(self):
+        return len(self.url)
+
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+extrator_url = ExtratorURL(url)
+print("Tamanho da URL:", len(extrator_url))
+
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
