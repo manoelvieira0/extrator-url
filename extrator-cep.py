f8870564dfca85360f743b0df4661ea13e17d0e0
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2022-02-21 07:28:15 -0300

    Quantificadores e intervalos

--- extrator-cep.py
+++ extrator-cep.py
@@ -2,7 +2,7 @@ endereco = "Praça Floriano Peixoto, 303, Centro, Plar, AL, 57150-000"
 
 import re #RegEx
 
-padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
 busca = padrao.search(endereco) #Match
 if busca:
     cep = busca.group()
